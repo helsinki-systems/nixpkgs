2604f67dfeba2ecbdddb2730ce52d61057d5408b
Ivar <ivar.scholten@protonmail.com>
2020-11-25 03:46:35 +0100

    lrzip: cleanup

--- pkgs/tools/compression/lrzip/default.nix
+++ pkgs/tools/compression/lrzip/default.nix
@@ -1,4 +1,4 @@
-{lib, stdenv, fetchurl, zlib, lzo, bzip2, lz4, nasm, perl}:
+{ lib, stdenv, fetchurl, zlib, lzo, bzip2, lz4, nasm, perl }:
 
 stdenv.mkDerivation rec {
   version = "0.641";
@@ -9,16 +9,17 @@ stdenv.mkDerivation rec {
     sha256 = "0ziyanspd96dc3lp2qdcylc7aq8dhb511jhqrhxvlp502fjqjqrc";
   };
 
-  buildInputs = [ zlib lzo bzip2 lz4 nasm perl ];
+  nativeBuildInputs = [ nasm perl ];
+  buildInputs = [ zlib lzo bzip2 lz4 ];
 
   configureFlags = [
     "--disable-asm"
   ];
 
-  meta = {
+  meta = with lib; {
     homepage = "http://ck.kolivas.org/apps/lrzip/";
     description = "The CK LRZIP compression program (LZMA + RZIP)";
-    license = lib.licenses.gpl2Plus;
-    platforms = lib.platforms.unix;
+    license = licenses.gpl2Plus;
+    platforms = platforms.unix;
   };
 }
